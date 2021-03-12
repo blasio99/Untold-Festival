a336a712fa203bd522a670ba10a4840792e8b770
Balázs Benedek <benedekabalazs@gmail.com>
2021-03-12 13:44:46 +0200

    Spring initializr - Gradle Project

---
+++ untoldfestival/src/test/java/dev/blasio99/untoldfestival/UntoldfestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.blasio99.untoldfestival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntoldfestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
